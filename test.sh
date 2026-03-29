88b8d430e70f995bb654bcb7b1ccbd32a2f32234
Vsevolod Pokhvalenko <119959398+xpokhv00@users.noreply.github.com>
2026-03-29 17:13:24 +0200

    update shebang in test.sh and simplify mpic++ and mpirun commands

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ $# -ne 1 ]; then
   exit 1
@@ -9,16 +9,15 @@ if ! [[ "$N" =~ ^[0-9]+$ ]] || [ "$N" -le 0 ]; then
   exit 1
 fi
 
-# nejmensi mocnina 2 >= N (kvuli stromove redukci)
 numProc=1
 while [ "$numProc" -lt "$N" ]; do
   numProc=$((numProc * 2))
 done
 
-mpic++ --prefix /usr/local/share/OpenMPI -O2 -Wall -Wextra -pedantic -std=c++17 -o mes mes.cpp
+mpic++ -O2 -Wall -Wextra -pedantic -std=c++17 -o mes mes.cpp
 
 dd if=/dev/random bs=1 count="$N" of=numbers 2>/dev/null
 
-mpirun --oversubscribe --prefix /usr/local/share/OpenMPI -np "$numProc" ./mes
+mpirun -np "$numProc" ./mes
 
 rm -f mes numbers
